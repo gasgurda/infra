7c391b9897ddb3ebf006e6ca3a07646a5685411b
Timur Eshpulatov <timur.eshpulatov@gmail.com>
2019-05-12 07:54:58 +0300

    test

--- startup.sh
+++ startup.sh
@@ -8,7 +8,8 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 7D2BAF1CF37B13E2069D6956105BD0E
 
 curl -sSL https://get.rvm.io | bash -s stable
 
-source ~/.rvm/scripts/rvm
+#source ~/.rvm/scripts/rvm
+source /etc/profile.d/rvm.sh
 
 rvm requirements
 
