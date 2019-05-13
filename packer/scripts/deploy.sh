b60f25499d1fb7b52bd32b78567188bf6fdbc245
Timur Eshpulatov <timur.eshpulatov@gmail.com>
2019-05-13 07:41:01 +0300

    Rewrite puma.service

--- packer/scripts/deploy.sh
+++ packer/scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-[[ -s "/home/appuser/.rvm/scripts/rvm" ]] && source "/home/appuser/.rvm/scripts/rvm"
+#[[ -s "/home/appuser/.rvm/scripts/rvm" ]] && source "/home/appuser/.rvm/scripts/rvm"
 cd /home/appuser
 git clone https://github.com/Artemmkin/reddit.git
-cd /home/appuser/reddit && bundle install
- 
-
+bash -lc "cd /home/appuser/reddit && bundle install"
+wget -O /etc/systemd/system/puma.service https://github.com/gasgurda/infra/blob/base-os-packer/packer/files/puma.service
+#systemctl enable puma.service
 
 
